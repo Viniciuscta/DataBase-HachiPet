42a4b6849b9dd89f572fb3e541e56a95b9c2d72e
Viniciuscta <viniciuscta123@gmail.com>
2024-04-23 19:25:53 -0300

    !

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var jsonServer = require('json-server')
-var server = jsonServer.create()
-var router = jsonServer.router('db.json')
-var middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, function () {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+server.listen(port);
